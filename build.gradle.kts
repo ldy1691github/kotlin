030ed30059b8d8b764fe590e6c3af73ebd726ce9
Vyacheslav Gerasimov <vyacheslav.gerasimov@jetbrains.com>
2020-02-24 23:48:35 +0300

    Build: Add localBuildCacheDirectory property to KotlinBuildProperties

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,7 @@ buildscript {
     dependencies {
         bootstrapCompilerClasspath(kotlin("compiler-embeddable", bootstrapKotlinVersion))
 
-        classpath("org.jetbrains.kotlin:kotlin-build-gradle-plugin:0.0.10")
+        classpath("org.jetbrains.kotlin:kotlin-build-gradle-plugin:0.0.12")
         classpath("com.gradle.publish:plugin-publish-plugin:0.9.7")
         classpath(kotlin("gradle-plugin", bootstrapKotlinVersion))
         classpath("org.jetbrains.dokka:dokka-gradle-plugin:0.9.17")
